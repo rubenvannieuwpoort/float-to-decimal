c9f7100ada7cc4efe33d1e1120466ad8a3c51d23
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2021-02-07 15:46:45 +0100

    Initial commit

---
+++ float-to-decimal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import struct
+
+def fp_to_decimal(binary_input, exponent_bits, mantissa_bits, num_fractional_digits, show_trailing_zeroes):
+	sign     = (binary_input & (1 << (exponent_bits + mantissa_bits))) >> (exponent_bits + mantissa_bits)
+	exponent = (binary_input & (((1 << exponent_bits) - 1) << mantissa_bits)) >> mantissa_bits
+	mantissa =  binary_input & ((1 << mantissa_bits) - 1)
+	if exponent != 0:
+		mantissa = mantissa + (1 << mantissa_bits)
+	if sign == 1: mantissa = -mantissa
+	return fraction_to_decimal(mantissa * (1 << exponent) , 1 << ((1 << (exponent_bits - 1)) + mantissa_bits - 1), num_fractional_digits, show_trailing_zeroes)
+
+def double_to_decimal(input, num_fractional_digits, show_trailing_zeroes = False):
+	binary_input = struct.unpack('Q', struct.pack('d', input))[0]
+	return fp_to_decimal(binary_input, 11, 52, num_fractional_digits, show_trailing_zeroes)
+
+def float_to_decimal(input, num_fractional_digits, show_trailing_zeroes = False):
+	binary_input = struct.unpack('I', struct.pack('f', input))[0]
+	return fp_to_decimal(binary_input, 8, 23, num_fractional_digits, show_trailing_zeroes)
+
+def fraction_to_decimal(numerator, denumerator, num_fractional_digits, show_trailing_zeroes = False):
+	sign = ''
+	if numerator < 0:
+		sign = '-'
+		numerator = -numerator
+	floor = numerator // denumerator
+	if num_fractional_digits == 0: return floor
+	numerator = numerator - floor * denumerator
+	if (numerator == 0 and not(show_trailing_zeroes)): return floor
+	fractional_digits = fractional_digits_from_proper_fraction(numerator, denumerator, num_fractional_digits, show_trailing_zeroes)
+	return sign + str(floor) + '.' + fractional_digits
+
+def fractional_digits_from_proper_fraction(numerator, denumerator, digits, show_trailing_zeroes):
+	if digits == 0 or (not(show_trailing_zeroes) and numerator == 0): return ''
+	numerator *= 10
+	digit = numerator // denumerator
+	rest_numerator = numerator - digit * denumerator
+	return str(digit) + fractional_digits_from_proper_fraction(rest_numerator, denumerator, digits - 1, show_trailing_zeroes)
